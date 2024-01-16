13717b2b81b5d4da4edbf295708cdfbb9a57eb30
ekonuma <konumaeike5618@gmail.com>
2024-01-16 13:31:10 -0300

    feat(init)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{padding: "50px"}}>
+      <h1>GTODO</h1>
+    </div>
+  );
+}
+
+export default App;
